76ca9a9a248756c43fe3d21569f413c44f321637
Hex-Zero <hex0@live.com>
2020-02-08 15:45:22 +0000

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+          string first = Console.ReadLine();
+          string second = Console.ReadLine();
+
+            Console.WriteLine(Program.Add(Int32.Parse(first), Int32.Parse(second)));
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+             return x + y;
+        }
+    }
+}
