5e7ad2a08d31d0e785c3007ada372efae6ced4d0
Hex-Zero <hex0@live.com>
2020-02-08 17:33:12 +0000

    better

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,61 +9,61 @@ namespace Calculator
     class Program
     {
 
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
 
-            //void FizzBuzz(int end , int start)
-            //{
-            //    int i = start;
-            //    string result = "";
-            //    if(i <= end)
-            //    {
-            //        if (i % 3 == 0)
-            //        {
-            //            result += "Fizz";
-            //        }
-            //        if (i % 5 == 0)
-            //        {
-            //            result += "Bizz";
-            //        }
-            //        if(result == "")
-            //        {
-            //            result = i.ToString();
-            //        }
-            //        Console.WriteLine(result);
-            //        i++;
-            //        FizzBuzz(end, i);
-            //    }
-            //}
+        //void FizzBuzz(int end , int start)
+        //{
+        //    int i = start;
+        //    string result = "";
+        //    if(i <= end)
+        //    {
+        //        if (i % 3 == 0)
+        //        {
+        //            result += "Fizz";
+        //        }
+        //        if (i % 5 == 0)
+        //        {
+        //            result += "Bizz";
+        //        }
+        //        if(result == "")
+        //        {
+        //            result = i.ToString();
+        //        }
+        //        Console.WriteLine(result);
+        //        i++;
+        //        FizzBuzz(end, i);
+        //    }
+        //}
 
-            //FizzBuzz(9161, 1);
+        //FizzBuzz(9161, 1);
 
-            string result = "";
-            for (int i = 1; i < 9161; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    result += "Fizz";
-                }
-                if (i % 5 == 0)
-                {
-                    result += "Buzz";
-                }
-                if (i % 7 == 0)
-                {
-                    result += "Ruzz";
-                }
-                if (result == "")
-                {
-                    result = i.ToString();
-                }
-                Console.WriteLine(result);
-                result = "";
+        //string result = "";
+        //for (int i = 1; i < 9161; i++)
+        //{
+        //    if (i % 3 == 0)
+        //    {
+        //        result += "Fizz";
+        //    }
+        //    if (i % 5 == 0)
+        //    {
+        //        result += "Buzz";
+        //    }
+        //    if (i % 7 == 0)
+        //    {
+        //        result += "Ruzz";
+        //    }
+        //    if (result == "")
+        //    {
+        //        result = i.ToString();
+        //    }
+        //    Console.WriteLine(result);
+        //    result = "";
 
-            }
-            Console.ReadLine();
-        }
+        //}
+        //Console.ReadLine();
+        //}
 
 
         //public enum ProductCodes
@@ -85,48 +85,50 @@ namespace Calculator
 
 
 
-        //    static void Main(string[] args)
-        //    {
-        //        Console.WriteLine("Type in the first number.");
-        //        string first = Console.ReadLine();
-        //        Console.WriteLine("Type in the the operand (Available operations: + - * /).");
-        //        string operation = Console.ReadLine();
-        //        Console.WriteLine("Type in the Second number.");
-        //        string second = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in the first number.");
+            string first = Console.ReadLine();
+            Console.WriteLine("Type in the the operand (Available operations: + - * /).");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Type in the Second number.");
+            string second = Console.ReadLine();
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(Program.Calculation(Int32.Parse(first), Int32.Parse(second), operation));
 
-        //        Console.WriteLine(Program.Calculation(Int32.Parse(first), Int32.Parse(second), operation));
-
-        //        Console.WriteLine("Type yes/y if you would like to Start again, else just press enter to exit.");
-        //        string endCall = Console.ReadLine();
-        //        if (endCall == "yes" || endCall == "y")
-        //        {
-        //            startAgain();
-        //        }
-        //    }
-        //    static string Calculation(int x, int y, string o)
-        //    {
-        //        if(o == "+"){
-        //            return (x + y).ToString();
-        //        }
-        //        else if (o == "-")
-        //        {
-        //            return (x - y).ToString();
-        //        }
-        //        else if (o == "/")
-        //        {
-        //            return (x / y).ToString();
-        //        }
-        //        else if (o == "*")
-        //        {
-        //            return (x * y).ToString();
-        //        }
-        //        else {
-        //            return "Not a valid operation.";
-        //        }
-        //    }
-        //    static void startAgain()
-        //    {
-        //        Main(null);
-        //    }
+            Console.WriteLine("Type yes/y if you would like to Start again, else just press enter to exit.");
+            string endCall = Console.ReadLine();
+            if (endCall == "yes" || endCall == "y")
+            {
+                startAgain();
+            }
+        }
+        static string Calculation(int x, int y, string o)
+        {
+            if (o == "+")
+            {
+                return (x + y).ToString();
+            }
+            else if (o == "-")
+            {
+                return (x - y).ToString();
+            }
+            else if (o == "/")
+            {
+                return (x / y).ToString();
+            }
+            else if (o == "*")
+            {
+                return (x * y).ToString();
+            }
+            else
+            {
+                return "Not a valid operation.";
+            }
+        }
+        static void startAgain()
+        {
+            Main(null);
+        }
     }
 }
