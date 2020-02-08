c15c9610f3437c6948b1f3ce1fb54cddc7d74cca
Hex-Zero <hex0@live.com>
2020-02-08 17:10:30 +0000

    Loops and Conditionals

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,7 +11,26 @@ namespace Calculator
 
         static void Main(string[] args)
         {
+            string result = "";
+            for(int i = 1; i< 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    result += "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+                if(result == "")
+                {
+                    result = i.ToString();
+                }
+                Console.WriteLine(result);
+                result = "";
 
+            }
+            Console.ReadLine();
         }
 
 
